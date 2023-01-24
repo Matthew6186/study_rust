0cb2baa7931a96f585410e260b62328acb516e3f
Matthew6186 <44461592+Matthew6186@users.noreply.github.com>
2023-01-24 10:15:30 +0900

    add debug folder

---
+++ project1/target/debug/deps/project1-6b9018f4175daf95.d
@@ -0,0 +1,5 @@
+/home/mtakahiro/Working/study_rust/project1/target/debug/deps/project1-6b9018f4175daf95.rmeta: src/main.rs
+
+/home/mtakahiro/Working/study_rust/project1/target/debug/deps/project1-6b9018f4175daf95.d: src/main.rs
+
+src/main.rs:
---
+++ project1/target/debug/deps/project1-6cc10a5b588582e9.d
@@ -0,0 +1,5 @@
+/home/mtakahiro/Working/study_rust/project1/target/debug/deps/project1-6cc10a5b588582e9.rmeta: src/main.rs
+
+/home/mtakahiro/Working/study_rust/project1/target/debug/deps/project1-6cc10a5b588582e9.d: src/main.rs
+
+src/main.rs:
